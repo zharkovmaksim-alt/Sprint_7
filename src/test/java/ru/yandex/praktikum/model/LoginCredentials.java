04ef2519f86f95bf464158350a025b7f781be747
Maksim <zharkov_maksim@mail.ru>
2026-07-17 17:29:50 +0300

    refactor: add Lombok to POJO classes

--- src/test/java/ru/yandex/praktikum/model/LoginCredentials.java
+++ src/test/java/ru/yandex/praktikum/model/LoginCredentials.java
@@ -1,30 +1,13 @@
 package ru.yandex.praktikum.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginCredentials {
     private String login;
     private String password;
-
-    public LoginCredentials() {
-    }
-
-    public LoginCredentials(String login, String password) {
-        this.login = login;
-        this.password = password;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/test/java/ru/yandex/praktikum/model/Order.java
+++ src/test/java/ru/yandex/praktikum/model/Order.java
@@ -1,7 +1,14 @@
 package ru.yandex.praktikum.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
     private String firstName;
     private String lastName;
@@ -12,92 +19,4 @@ public class Order {
     private String deliveryDate;
     private String comment;
     private List<String> color;
-
-    public Order() {
-    }
-
-    public Order(String firstName, String lastName, String address, String metroStation,
-                 String phone, int rentTime, String deliveryDate, String comment, List<String> color) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.metroStation = metroStation;
-        this.phone = phone;
-        this.rentTime = rentTime;
-        this.deliveryDate = deliveryDate;
-        this.comment = comment;
-        this.color = color;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getMetroStation() {
-        return metroStation;
-    }
-
-    public void setMetroStation(String metroStation) {
-        this.metroStation = metroStation;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public int getRentTime() {
-        return rentTime;
-    }
-
-    public void setRentTime(int rentTime) {
-        this.rentTime = rentTime;
-    }
-
-    public String getDeliveryDate() {
-        return deliveryDate;
-    }
-
-    public void setDeliveryDate(String deliveryDate) {
-        this.deliveryDate = deliveryDate;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public List<String> getColor() {
-        return color;
-    }
-
-    public void setColor(List<String> color) {
-        this.color = color;
-    }
 }
